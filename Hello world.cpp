36010c54bbe6f28713d4d2f86114f25cf1cd00c5
Hanmur <73321642+Hanmur@users.noreply.github.com>
2021-03-01 19:56:00 +0800

    Create Hello world.cpp

---
+++ Hello world.cpp
@@ -0,0 +1,12 @@
+/.            Hanmur          ./
+/.           2021.1.1         ./
+/.         Hello World        ./
+
+#include<iostream>
+using namespace std;  //set the std as the default
+
+int main(){
+	cout << "Hello World";    //print Hello world
+	
+	return 0;           //end the file
+}
