dfb920060c14dbae2e7f143b43f70e3b1236215f
Hanmur <73321642+Hanmur@users.noreply.github.com>
2021-04-21 10:16:29 +0800

    Create Hello world.java

---
+++ Hello world.java
@@ -0,0 +1,9 @@
+/*            Hanmur          */
+/*           2021.4.21        */
+/*         Hello World        */
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
